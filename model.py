41ec9bf960308f78fba8c2cda3626ef2dad64ef5
Srividya <bhuvanagirisrividya@gmail.com>
2025-01-05 20:44:42 +0530

    skeleton code

---
+++ model.py
@@ -0,0 +1,11 @@
+import pickle
+from sklearn.linear_model import LinearRegression
+from sklearn.datasets import make_regression
+
+def train_model():
+    X, y = make_regression(n_samples=100, n_features=1, noise=0.1)
+    model = LinearRegression()
+    model.fit(X, y)
+    with open('model.pkl', 'wb') as f:
+        pickle.dump(model, f)
+    return model
---
+++ test_model.py
@@ -0,0 +1,9 @@
+import pickle
+from model import train_model
+
+def test_model_training():
+    model = train_model()
+    assert model is not None
+    with open('model.pkl', 'rb') as f:
+        loaded_model = pickle.load(f)
+    assert loaded_model is not None
