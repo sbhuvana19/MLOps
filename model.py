c82b92182c32aec05aaca35a5aff785d112abd0e
Srividya <bhuvanagirisrividya@gmail.com>
2025-01-24 08:15:43 +0530

    reverted model.py changes

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import pickle
 from sklearn.linear_model import LinearRegression
 from sklearn.datasets import make_regression
 
+
 def train_model():
     X, y = make_regression(n_samples=100, n_features=1, noise=0.1)
     model = LinearRegression()
