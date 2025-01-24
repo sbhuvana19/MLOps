21f3fe19dbe695673977de8e3b6cc00a1435de77
Srividya <bhuvanagirisrividya@gmail.com>
2025-01-24 08:22:47 +0530

    Fixed linting issues

--- model.py
+++ model.py
@@ -7,7 +7,6 @@ def train_model():
     X, y = make_regression(n_samples=100, n_features=1, noise=0.1)
     model = LinearRegression()
     model.fit(X, y)
-    with open('model.pkl', 'wb') as f:
+    with open("model.pkl", "wb") as f:
         pickle.dump(model, f)
     return model
-    
