9b7d9f86f0556d698750a16d46587118b51e5547
Srividya <bhuvanagirisrividya@gmail.com>
2025-01-25 17:39:44 +0530

    Fixed linting issues

--- model.py
+++ model.py
@@ -1,10 +1,10 @@
-from sklearn.linear_model import Ridge
 import numpy as np
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.metrics import mean_squared_error
+import pandas as pd
 import joblib
 import mlflow
-import pandas as pd
+from sklearn.linear_model import Ridge
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import LabelEncoder
 
 
@@ -33,12 +33,21 @@ def train_model():
         param_grid = {"alpha": np.logspace(-4, 4, 10)}
 
         # Perform Grid Search with cross-validation
-        grid_search = GridSearchCV(model, param_grid, scoring="r2", cv=5)
+        grid_search = GridSearchCV(
+            model,
+            param_grid,
+            scoring="r2",
+            cv=5
+        )
         grid_search.fit(X_train, y_train)
 
         # Log the best parameters and metrics to MLflow
         mlflow.log_param("model", "Ridge")
-        mlflow.log_param("best_alpha", float(grid_search.best_params_["alpha"]))
+        best_alpha = float(
+            grid_search.best_params_["alpha"]
+        )  # Extract best alpha
+        mlflow.log_param("best_alpha", best_alpha)
+
         mlflow.log_metric("best_r2_score", grid_search.best_score_)
 
         # Use the best estimator for predictions
@@ -48,18 +57,22 @@ def train_model():
         # Evaluate and log the test MSE
         mse = mean_squared_error(y_test, y_pred)
         mlflow.log_metric("test_mse", mse)
-        print(f"Mean Squared Error on test set: {mse}")
+        print(
+            "Mean Squared Error on test set: "
+            + str(mse)
+        )
 
         # Save the best model to a file
         joblib.dump(best_model, "model.joblib")
 
         # Display the best parameters and results
         best_params = {
-            key: float(value) for key, value in grid_search.best_params_.items()
+         key: float(value) for key, value in grid_search.best_params_.items()
         }
         print("Best Parameters:", best_params)
         print("Best R2 Score (CV):", grid_search.best_score_)
 
 
 # Run the training function
-train_model()
+if __name__ == "__main__":
+    train_model()
